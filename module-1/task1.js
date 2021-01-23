512c91c6e06bea2a34098d7412e00a22a6f4d2d6
Ivanetrenko673 <dufschmirz@gmail.com>
2021-01-23 18:51:38 +0200

    added changes

---
+++ module-1/task1.js
@@ -0,0 +1,34 @@
+const str = "Привіт"; //string
+console.log (str);
+
+const num = 5; //number
+console.log (num);
+
+const flag = true; //boolean
+console.log (flag);
+
+const txt = "true"; //string
+console.log (txt);
+
+
+
+const userName = "Ivan";
+
+const userAge = 13;
+
+const userSurname = "Hobi";
+
+const userCar = "Mercedes";
+
+const userStreetNumber = 25 ;
+
+
+let userEmeil = "hfdfyv@gmail.com";
+
+let userEmailPasword = "qwerty";
+
+let userSchoolClass = 11;
+
+let userID = "235425get46";
+
+let userBirsday = "18.30.2000";
