54a0170644d345a40ae46f81948c1f0006dc08db
Ivanetrenko673 <dufschmirz@gmail.com>
2021-03-25 20:56:55 +0200

    sfd

--- module-1/practiceWork4/task_4.js
+++ module-1/practiceWork4/task_4.js
@@ -1,9 +1,16 @@
 const number = (Math.random() * (5 - 1) + 1);
 
-const numbers = Math.round(number) ;
+const numbers = Math.round(number)
 
-if ( numbers >= 1 && numbers <= 5) {
- console.log(numbers);
-} else {
-    console.log("...?")
-}
+if(numbers === 1){
+        console.log('цифра 1');
+    } else if (numbers === 2) {
+        console.log('цифра 2');
+    } else if (numbers === 3 ) {
+        console.log('цифра 3');  
+    } else if (numbers === 4 ) {
+        console.log('цифра 4');  
+    } else if (numbers === 5 ) {
+        console.log('цифра 5');  
+    }
+
